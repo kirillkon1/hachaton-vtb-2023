6424418848ad376d4f951cf502f29592c7de62e3
kirillkon1 <kirill.kondrashow@mail.ru>
2023-10-09 15:20:41 +0300

    git init

---
+++ src/main/java/ru/vtb/vtbbackend/VtbBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.vtb.vtbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtbBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vtb/vtbbackend/VtbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vtb.vtbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
