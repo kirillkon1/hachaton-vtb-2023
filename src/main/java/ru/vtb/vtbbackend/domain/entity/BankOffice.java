5fcae49a0ed1afedc0638a48777221427a6fbc25
ChibiaSad <magur1997@gmail.com>
2023-10-13 21:34:19 +0300

    atm entity added

--- src/main/java/ru/vtb/vtbbackend/domain/entity/BankOffice.java
+++ src/main/java/ru/vtb/vtbbackend/domain/entity/BankOffice.java
@@ -22,13 +22,13 @@ public class BankOffice {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         BankOffice that = (BankOffice) o;
-        return getId() != null && Objects.equals(getId(), that.getId());
+        return Objects.equals(id, that.id);
     }
 
     @Override
     public int hashCode() {
-        return getClass().hashCode();
+        return Objects.hash(id);
     }
 }
