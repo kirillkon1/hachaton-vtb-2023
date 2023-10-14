bb855ceb11310643f096a8051859f97adb2ce1af
kirillkon1 <kirill.kondrashow@mail.ru>
2023-10-14 14:41:49 +0300

    BankLoad created

--- src/main/java/ru/vtb/vtbbackend/domain/entity/BankLoad.java
+++ src/main/java/ru/vtb/vtbbackend/domain/entity/BankLoad.java
@@ -1,15 +1,31 @@
 package ru.vtb.vtbbackend.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class BankLoad {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
+    @JsonIgnore
     private Long id;
 
-    private Long loadFactor;
+    private Double load;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "bank_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Bank bank;
 
 }
---
+++ src/main/java/ru/vtb/vtbbackend/domain/repository/BankLoadRepository.java
@@ -0,0 +1,21 @@
+package ru.vtb.vtbbackend.domain.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.vtb.vtbbackend.domain.entity.BankLoad;
+
+import java.util.Optional;
+
+public interface BankLoadRepository extends JpaRepository<BankLoad, Long> {
+
+
+    @Query(value = "select l from BankLoad l where l.bank.id = :id ")
+    Page<BankLoad> findPageLoadByBandId(@Param("id") Long bankId, Pageable pageable);
+
+
+    @Query(value = "select l from BankLoad l where l.bank.id = :id order by l.dateTime DESC limit 1")
+    Optional<BankLoad> findLastLoadByBankId(@Param("id") Long bankId);
+}
